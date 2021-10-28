b743baedc16ccdebdb58381babfc656146be6cca
WooYong Kim <wykim970@gmail.com>
2021-10-28 23:31:09 +0900

    Movies rendering

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component{
   state = {
@@ -8,7 +10,9 @@ class App extends React.Component{
   };
 
   getMovies = async () => {
-    const {data: {data: {movies}}} = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json")
+    const {data: {data: {movies}}} = await axios.get(
+      "https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating"
+      )
     console.log(movies);
     this.setState({ movies, isLoading: false });
   }
@@ -18,12 +22,28 @@ class App extends React.Component{
   }
 
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
 
     return (
-      <div>
-        {isLoading ? "Loading..." : "Ready"}
-      </div>
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie 
+                key={movie.id} 
+                year={movie.year} 
+                title={movie.title} 
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres} />
+            ))}
+          </div>
+        )}
+      </section>
     )
   }
 }
